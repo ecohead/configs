ecca1de9f3127cf7d84df9be22c695785b230704
Aurélien Devaux <github@aureldvx.net>
2025-08-12 00:02:30 +0200

    style(apps): react-vite-ts

--- apps/vite-react-ts/src/main.tsx
+++ apps/vite-react-ts/src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App.tsx';
 
 createRoot(document.querySelector('#root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<App />
+	</StrictMode>,
 );
